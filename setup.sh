c462d71f876dc7a8e7f406d4abf5c420698ff1c5
Lukas Martinelli <me@lukasmartinelli.ch>
2014-09-02 17:52:25 +0200

    Added custom gtk themes

--- setup.sh
+++ setup.sh
@@ -18,3 +18,8 @@ ln -nfsv $CWD/.Xresources $HOME
 #Setup i3 tiling wm
 ln -nfsv $CWD/.i3 $HOME
 ln -nfsv $CWD/.i3status.conf $HOME
+
+#Setup custom gtk themes
+ln -nfsv $CWD/.gtkrc-2.0 $HOME
+mkdir -p $HOME/.config
+ln -nfsv $CWD/gtk-3.0 $HOME/.config
