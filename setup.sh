944972562538dab9d407db5fc3e16deb741d8791
Lukas Martinelli <me@lukasmartinelli.ch>
2015-08-28 11:53:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,16 +10,3 @@ ln -nfsv $CWD/.vimrc $HOME
 
 #Zsh config
 ln -nfsv $CWD/.zshrc $HOME
-
-#Setup xorg
-ln -nfsv $CWD/.xinitrc $HOME
-ln -nfsv $CWD/.Xresources $HOME
-
-#Setup i3 tiling wm
-ln -nfsv $CWD/.i3 $HOME
-ln -nfsv $CWD/.i3status.conf $HOME
-
-#Setup custom gtk themes
-ln -nfsv $CWD/.gtkrc-2.0 $HOME
-mkdir -p $HOME/.config
-ln -nfsv $CWD/gtk-3.0 $HOME/.config
